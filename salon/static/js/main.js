8f151189b6cb1fd808d017fc6a47678a9fce3810
Luciano Lima <ccll4ever1@Lucianos-MacBook-Pro.local>
2020-10-24 19:17:21 +0100

    Styling home page

---
+++ salon/static/js/main.js
@@ -0,0 +1,20 @@
+ScrollReveal().reveal('.amo-logo', {
+    delay: 1000,
+    // origin: 'left',
+    distance: '50px',
+    easing: 'ease-in',
+    
+})
+
+ScrollReveal().reveal('.collapsed-text', {
+    origin: 'top',
+    easing: 'ease-in',
+    distance: '50px',
+    
+})
+
+
+
+
+
+
