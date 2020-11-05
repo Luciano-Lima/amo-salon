4c25563298bd2d1039be9748587a2a1e20002c6c
Luciano Lima <ccll4ever1@Lucianos-MacBook-Pro.local>
2020-11-05 17:45:48 +0000

    Adding form validation

--- salon/forms.py
+++ salon/forms.py
@@ -1,7 +1,11 @@
 from  django import forms
 from django.core.exceptions import ValidationError
+from django.core.validators import MinLengthValidator, EmailValidator, RegexValidator
+
 
 class ContactForm(forms.Form):
-    name = forms.CharField(max_length=100, required=True)
-    email = forms.EmailField(required=True)
-    message = forms.CharField(required=True, widget=forms.Textarea)
+    name = forms.CharField(validators=[MinLengthValidator(3), RegexValidator(r'^[a-zA-Z]*$', 'That is not a valid name.')], max_length=100)
+    email = forms.EmailField(validators=[EmailValidator])
+    message = forms.CharField(validators=[MinLengthValidator(20, 'Message need to be at least 20 characters long! '),], widget=forms.Textarea)
+
+    
