a2519ce4bfe548cd4c126a2c08d6bddbcb940e5e
Luciano Lima <ccll4ever1@Lucianos-MacBook-Pro.local>
2020-11-06 00:22:36 +0000

    Correcting validator error message on contact form

--- salon/forms.py
+++ salon/forms.py
@@ -4,8 +4,8 @@ from django.core.validators import MinLengthValidator, EmailValidator, RegexVali
 
 
 class ContactForm(forms.Form):
-    name = forms.CharField(validators=[MinLengthValidator(3), RegexValidator(r'^[a-zA-Z]*$', 'That is not a valid name.')], max_length=100)
+    name = forms.CharField(validators=[MinLengthValidator(3, 'Needs to be at least 3 characters long! '), RegexValidator(r'^[a-zA-Z]*$', 'That is not a valid name.')], max_length=100)
     email = forms.EmailField(validators=[EmailValidator])
-    message = forms.CharField(validators=[MinLengthValidator(20, 'Message need to be at least 20 characters long! '),], widget=forms.Textarea)
+    message = forms.CharField(validators=[MinLengthValidator(20, 'Needs to be at least 20 characters long! '),], widget=forms.Textarea)
 
     
