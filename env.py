be609605f639d89e9dba71a8bd436aedd0d58525
Luciano Lima <ccll4ever1@Lucianos-MacBook-Pro.local>
2020-11-11 22:59:09 +0000

    Updated aws config vars

--- env.py
+++ env.py
@@ -1,9 +1,9 @@
 import os
 
 os.environ['SECRET_KEY'] = '7%9ol=71v&p02%ar&@@2zcj^&2%l_)qrha2fl$0rig_ocuiu%@'
-os.environ['AWS_ACCESS_KEY_ID'] = 'AKIAS6HODWQOP7MTPF67'
-os.environ['AWS_SECRET_ACCESS_KEY'] = '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'
-os.environ['AWS_STORAGE_BUCKET_NAME'] = 'amo-hair-beauty-salon'
+os.environ['AWS_ACCESS_KEY_ID'] = 'AKIAS6HODWQOAVUBWS7L'
+os.environ['AWS_SECRET_ACCESS_KEY'] = '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'
+os.environ['AWS_STORAGE_BUCKET_NAME'] = 'amo-salon'
 
 
 
