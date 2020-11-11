5a486b28919a4cf982b51918ea8a869a0f608932
Luciano Lima <ccll4ever1@Lucianos-MacBook-Pro.local>
2020-11-11 13:48:54 +0000

    Setting aws s3 config

--- amo_salon/settings.py
+++ amo_salon/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'salon',
     'crispy_forms',
+    'storages',
 ]
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
@@ -152,3 +153,11 @@ EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
+
+# Aws variables
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
+
+AWS_S3_FILE_OVERWRITE = False
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
--- env.py
+++ env.py
@@ -1,10 +1,10 @@
 import os
 
 os.environ['SECRET_KEY'] = '7%9ol=71v&p02%ar&@@2zcj^&2%l_)qrha2fl$0rig_ocuiu%@'
-# os.environ['EMAIL_HOST'] = 'smtp.gmail.com'
-# os.environ['EMAIL_PORT'] = '587'
-# os.environ['EMAIL_HOST_USER'] = 'freelance.website.manager@gmail.com'
-# os.environ['EMAIL_HOST_PASSWORD'] = 'Tininha29--'
+os.environ['AWS_ACCESS_KEY_ID'] = 'AKIAS6HODWQOP7MTPF67'
+os.environ['AWS_SECRET_ACCESS_KEY'] = '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'
+os.environ['AWS_STORAGE_BUCKET_NAME'] = 'amo-hair-beauty-salon'
+
 
 
 os.environ['EMAIL_HOST'] = 'smtp.gmail.com'
