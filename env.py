7c90abeb088a1365bdd506fcca00dbeb52f28852
Luciano Lima <ccll4ever1@Lucianos-MacBook-Pro.local>
2020-11-12 20:27:04 +0000

    update to the aws bucket

--- env.py
+++ env.py
@@ -1,12 +1,14 @@
 import os
-
+# django 
 os.environ['SECRET_KEY'] = '7%9ol=71v&p02%ar&@@2zcj^&2%l_)qrha2fl$0rig_ocuiu%@'
-os.environ['AWS_ACCESS_KEY_ID'] = 'AKIAS6HODWQOAVUBWS7L'
-os.environ['AWS_SECRET_ACCESS_KEY'] = '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'
-os.environ['AWS_STORAGE_BUCKET_NAME'] = 'amo-salon'
+
+# AWS bucket
+os.environ['AWS_ACCESS_KEY_ID'] = 'AKIAS6HODWQOJCYW434T'
+os.environ['AWS_SECRET_ACCESS_KEY'] = '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'
+os.environ['AWS_STORAGE_BUCKET_NAME'] = 'amo-beauty-salon'
 
 
-
+# Gmail
 os.environ['EMAIL_HOST'] = 'smtp.gmail.com'
 os.environ['EMAIL_PORT'] = '587'
 os.environ['EMAIL_HOST_USER'] = 'freelance.website.manager@gmail.com'
