de547d7fc5b10c0eaad63138c0e0f7d47e034d30
Luciano Lima <ccll4ever1@Lucianos-MacBook-Pro.local>
2020-11-13 23:03:08 +0000

    x

--- amo_salon/settings.py
+++ amo_salon/settings.py
@@ -31,8 +31,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = (os.environ.get('DEBUB_VALUE') == 'True')
-DEBUG = False
+# DEBUG = (os.environ.get('DEBUB_VALUE') == 'True')
+DEBUG = True
 
 ALLOWED_HOSTS = ['amo-hair-beauty.herokuapp.com', '127.0.0.1']
 
