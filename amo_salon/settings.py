000428821e6db2931063f904758ac8dcb8a335d0
Luciano Lima <ccll4ever1@Lucianos-MacBook-Pro.local>
2020-11-15 19:00:20 +0000

    Static file

--- amo_salon/settings.py
+++ amo_salon/settings.py
@@ -140,6 +140,10 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')#Heroku will use this path to collect the static files 
 STATIC_URL = '/static/'
 
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
